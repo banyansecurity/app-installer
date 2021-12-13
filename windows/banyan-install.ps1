9f683c5f5ed5c775feeed1f25e959e93f7760a35
fjamal15 <32148999+fjamal15@users.noreply.github.com>
2021-12-13 15:56:15 -0800

    Update banyan-install.ps1
    Added the ability to autocreate mdm-config.json. Included lines to auto pull email address for Intune use cases

--- windows/banyan-install.ps1
+++ windows/banyan-install.ps1
@@ -1,27 +1,35 @@
-function Delete() {
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-    $Path = ".\Banyan-Install-2.0.ps1"
-    Write-Host $Path 
-    Remove-Item $Path -force
-} 
+
+###Enter Banyan Application Version (Example: 2.5.0)
+$VERSION="X.Y.Z"
 
 # Create Folders
 New-Item -Path "C:\ProgramData" -Name "banyantemp" -ItemType "directory" -Force
 New-Item -Path "C:\ProgramData" -Name "Banyan" -ItemType "directory" -Force
 
-# Copy Json file saved to the current directory of this script
-Copy-Item .\mdm-config.json -Destination C:\ProgramData\Banyan\ -Force
+# For Intune Deployments - Obtains email of user from Intune registry entry
+
+$ADJoinInfo = Get-ChildItem -path HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\JoinInfo
+$ADJoinInfo = $ADJoinInfo -replace "HKEY_LOCAL_MACHINE","HKLM:"
+$User = Get-ItemProperty -Path $ADJoinInfo
+$Email = $User.UserEmail
+echo $Email
+sleep 5
+
+# Creates mdm-config.json with the appropriate values
+$json = @{ mdm_invite_code = "INVITECODE"; mdm_deploy_user = $Email; mdm_deploy_email = $Email; mdm_start_at_boot = $true; mdm_present = $true; mdm_vendor_name = "Intune" } | ConvertTo-Json
+Set-Content -Path C:\ProgramData\Banyan\mdm-config.json -Value $json -NoNewLine
+
 
 # Download Banyan Install file
 $progressPreference = 'silentlyContinue'
-invoke-webrequest "https://www.banyanops.com/app/releases/Banyan-Setup-2.5.0.exe" -outfile "C:\ProgramData\banyantemp\Banyan-Setup-2.5.0.exe" -UseBasicParsing
+invoke-webrequest "https://www.banyanops.com/app/releases/Banyan-Setup-"$VERSION".exe" -outfile "C:\ProgramData\banyantemp\Banyan-Setup-"$VERSION".exe" -UseBasicParsing
 $progressPreference = 'Continue'
 
 # Install install-module RunAsUser 
 install-module RunAsUser -Force
 
 # Install Application
-Start-Process C:\ProgramData\banyantemp\Banyan-Setup-2.5.0.exe '/S' -Wait
+Start-Process C:\ProgramData\banyantemp\Banyan-Setup-"$VERSION".exe '/S' -Wait
 & 'C:\Program Files\Banyan\Banyan.exe' --staged-deploy-key={INSERT DEPLOY KEY}
 sleep 10
 
@@ -43,4 +51,11 @@ Uninstall-Module RunAsUser -Force
 Remove-Item -Path C:\ProgramData\banyantemp -Recurse -Force
 
 # Deletes script off device
+
+function Delete() {
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    $Path = ".\Banyan-Install-2.0.ps1"
+    Write-Host $Path 
+    Remove-Item $Path -force
+} 
 Delete
