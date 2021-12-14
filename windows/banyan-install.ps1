7f76d55b06cf15a5551f62cf4bf148a7a24d0a9b
Tarun Desikan <tarun@banyansecurity.io>
2021-12-14 12:10:34 -0800

    macos - refactor

--- windows/banyan-install.ps1
+++ windows/banyan-install.ps1
@@ -1,63 +1,122 @@
-###Enter Banyan Application Version (Example: 2.5.0)
-$VERSION="X.Y.Z"
+# Run as administrator
 
-function create_dir() {
-    # Create Folders
-    New-Item -Path "C:\ProgramData" -Name "banyantemp" -ItemType "directory" -Force
-    New-Item -Path "C:\ProgramData" -Name "Banyan" -ItemType "directory" -Force
+$APP_VERSION=$args[0]
+$INVITE_CODE=$args[1]
+$DEPLOY_KEY=$args[2]
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (! $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Error "This script must be run as Administrator"   
+    exit 1
+} else {
+    Write-Host "Installing app"
+}
+
+if (!$APP_VERSION -or !$INVITE_CODE -or !$DEPLOY_KEY) {
+    Write-Host "Usage: "
+    Write-Host "$PSCommandPath <APP_VERSION> <INVITE_CODE> <DEPLOY_KEY>"   
+    exit 1
+} else {
+    Write-Host "Installing app version: $APP_VERSION"
+    Write-Host "Installing with invite code: $INVITE_CODE"
+    Write-Host "Installing using deploy key: $DEPLOY_KEY"
+}
+
+$global_profile_dir = "C:\ProgramData"
+
+function create_config() {
+    Write-Host "Creating mdm-config json file"
+
+    $banyan_dir_name = "Banyan"
+    $global_config_dir = $global_profile_dir + "\" + $banyan_dir_name
+    $global_config_file = $global_config_dir + "\" + "mdm-config.json"
+
     # For Intune Deployments - Obtains email of user from Intune registry entry
-    $ADJoinInfo = Get-ChildItem -path HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\JoinInfo
-    $ADJoinInfo = $ADJoinInfo -replace "HKEY_LOCAL_MACHINE","HKLM:"
-    $User = Get-ItemProperty -Path $ADJoinInfo
-    $Email = $User.UserEmail
-    echo $Email
-    sleep 5
-    # Creates mdm-config.json with the appropriate values
-    $json = @{ mdm_invite_code = "INVITECODE"; mdm_deploy_user = $Email; mdm_deploy_email = $Email; mdm_start_at_boot = $true; mdm_present = $true; mdm_vendor_name = "Intune" } | ConvertTo-Json
-    Set-Content -Path C:\ProgramData\Banyan\mdm-config.json -Value $json -NoNewLine
+    $intune_info = "HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\JoinInfo"
+    $intune_user = ""
+    $intune_email = ""
+    if (Test-Path $intune_info) {
+        Write-Host "Intune deployment - extracting user email"
+        $ADJoinInfo = Get-ChildItem -path $intune_info
+        $ADJoinInfo = $ADJoinInfo -replace "HKEY_LOCAL_MACHINE","HKLM:"
+        $ADJoinUser = Get-ItemProperty -Path $ADJoinInfo
+        $intune_email = $User.UserEmail
+        $intune_user = $intune_email.Split("@")[0]
+        Write-Host "Intune deployment - found user - $intune_email, $intune_user"
+    }
+
+    $json = [pscustomobject]@{ 
+        mdm_invite_code = $INVITE_CODE
+        mdm_deploy_user = $intune_user
+        mdm_deploy_email = $intune_email 
+        mdm_device_ownership = "C"
+        mdm_present = $true
+        mdm_vendor_name = "Intune"
+        mdm_start_at_boot = $true
+        mdm_hide_on_start = $true  
+
+    } | ConvertTo-Json
+
+    New-Item -Path $global_profile_dir -Name $banyan_dir_name -ItemType "directory" -Force
+    Set-Content -Path $global_config_file -Value $json -NoNewLine
 }
 
 function download_extract() {
-    # Download Banyan Install file
-    $progressPreference = 'silentlyContinue'
-    invoke-webrequest "https://www.banyanops.com/app/releases/Banyan-Setup-"$VERSION".exe" -outfile "C:\ProgramData\banyantemp\Banyan-Setup-"$VERSION".exe" -UseBasicParsing
-    $progressPreference = 'Continue'
+    Write-Host "Downloading installer EXE"    
+
+    $tmp_dir_name = "banyantemp"
+    $tmp_dir = $global_profile_dir + "\" + $tmp_dir_name
+
+    New-Item -Path $global_profile_dir -Name $tmp_dir_name -ItemType "directory" -Force
+
+    $dl_file = $tmp_dir + "\" + "Banyan-Setup-$APP_VERSION.exe"
+
+    if (Test-Path $dl_file -PathType leaf) {
+        Write-Host "Installer EXE already downloaded"    
+    } else {
+        $progressPreference = 'silentlyContinue'
+        Invoke-Webrequest "https://www.banyanops.com/app/releases/Banyan-Setup-$APP_VERSION.exe" -outfile $dl_file -UseBasicParsing
+        $progressPreference = 'Continue'
+    }
+
     # Install Application
-    Start-Process C:\ProgramData\banyantemp\Banyan-Setup-"$VERSION".exe '/S' -Wait
+    Start-Process -FilePath $dl_file -ArgumentList "/S" -Wait
 }
 
 function stage() {
-    Start-Process C:\Program Files\Banyan\Banyan.exe '--staged-deploy-key={INSERT DEPLOY KEY}'
-    sleep 10
+    Write-Host "Running staged deployment"
+    Start-Process -FilePath "C:\Program Files\Banyan\Banyan.exe" -ArgumentList "--staged-deploy-key=$DEPLOY_KEY" -Wait
+    Write-Host "Staged deployment done. Have the user start the Banyan app to complete registration."
 }
 
-function start() {
-    # Install install-module RunAsUser 
-    install-module RunAsUser -Force
+function start_app() {
+    Write-Host "Starting the Banyan app as current user"
 
-    # Open App as Current User
+    Install-Module RunAsUser -Force
+
     $scriptblock = {
         & 'C:\Program Files\Banyan\Banyan.exe'
     }
 
-    try{
+    try {
         Invoke-AsCurrentUser -scriptblock $scriptblock
     } catch {
-        write-error "Something went wrong"
+        Write-Error "Couldn't start Banyan app"
     }
     sleep 10
 
-    # Uninstall install-module RunAsUser 
     Uninstall-Module RunAsUser -Force
 }
 
-create_dir
-download_extract
-stage
-launch
+function stop_app() {
+    Write-Host "Stopping Banyan app"
+    Stop-Process -Name Banyan -Force
+}
 
-
-
+#create_config
+#download_extract
+#stage
+start_app
 
 
 
