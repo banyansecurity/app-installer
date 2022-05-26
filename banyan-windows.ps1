47de1273ca8c7ba2abbc12b6a300c90dc50fb0fc
kunalsanghavi20 <71561795+kunalsanghavi20@users.noreply.github.com>
2022-05-26 10:44:03 -0700

    Update banyan-windows.ps1

--- banyan-windows.ps1
+++ banyan-windows.ps1
@@ -104,7 +104,7 @@ function download_install() {
 
 function stage() {
     Write-Host "Running staged deployment"
-    Start-Process -FilePath "C:\Program Files\Banyan\Banyan.exe" -ArgumentList "--staged-deploy-key=$DEPLOYMENT_KEY" -Wait
+    Start-Process -FilePath "C:\Program Files\Banyan\resources\bin\banyanapp-admin.exe" -ArgumentList "stage --key=$DEPLOYMENT_KEY" -Wait
     Write-Host "Staged deployment done. Have the logged_on_user start the Banyan app to complete registration."
 }
 
