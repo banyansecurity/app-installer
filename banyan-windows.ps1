7c1d06ab8afaf1fc29927cf689e617ced678128d
fjamal15 <32148999+fjamal15@users.noreply.github.com>
2022-06-29 15:18:02 -0700

    Update banyan-windows.ps1
    hide on start = true

--- banyan-windows.ps1
+++ banyan-windows.ps1
@@ -77,7 +77,7 @@ function create_config() {
         mdm_hide_services = $false
         mdm_disable_quit = $false
         mdm_start_at_boot = $true
-        mdm_hide_on_start = $false
+        mdm_hide_on_start = $true
     } | ConvertTo-Json
 
     New-Item -Path $global_profile_dir -Name $banyan_dir_name -ItemType "directory" -Force | Out-Null
