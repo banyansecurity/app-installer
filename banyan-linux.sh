18abbfd6c16d840e5da0c2f041c5ff840197ca14
Naresh Kakubal <naresh@banyansecurity.io>
2025-03-28 00:26:02 -0700

    Update banyan-linux.sh

--- banyan-linux.sh
+++ banyan-linux.sh
@@ -20,7 +20,8 @@ IS_MANAGED_DEVICE=false
 DEVICE_MANAGER_NAME=""
 HIDE_SERVICES=false
 DISABLE_QUIT=false
-START_AT_BOOT=false
+START_AT_BOOT=true
+AUTO_LOGIN=true
 HIDE_ON_START=false
 DISABLE_AUTO_UPDATE=false
 
@@ -82,6 +83,7 @@ function create_config() {
         "mdm_hide_services": '"${HIDE_SERVICES}"',
         "mdm_disable_quit": '"${DISABLE_QUIT}"',
         "mdm_start_at_boot": '"${START_AT_BOOT}"',
+        "mdm_auto_login": '"${AUTO_LOGIN}"',
         "mdm_hide_on_start": '"${HIDE_ON_START}"',
         "mdm_disable_auto_update": '"${DISABLE_AUTO_UPDATE}"'
     }'
